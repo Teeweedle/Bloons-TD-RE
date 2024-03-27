674d56ac7345b0fdc941b1d31b1d05e107e3f590
Teeweedle <102382922+Teeweedle@users.noreply.github.com>
2024-03-27 09:11:51 -0400

    Bug Fixes
    Fixed bug with path drawing (no more duplicates. Fixed bug not being able to path to the last waypoint. Fixed bug drawing lines to the anchor points of the last waypoint in the list.

--- Assets/Scripts/BloonMovement.cs
+++ Assets/Scripts/BloonMovement.cs
@@ -4,49 +4,35 @@ using UnityEngine;
 
 public class BloonMovement : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> _path;//TODO: Update to get path dynamically
-
+    [SerializeField] private List<Vector2> _path;
     private float _speed = 5f;
-    private float _duration, _startTime, _fraction;
     private int _currentPosition;
     
     private void Start()
     {
+        //TODO: Change so path is passed during instantiation rather than searched for
+        _path = GameObject.Find("Bloon Path").GetComponent<BloonPathCreator>().GetPathVectors();
         _currentPosition = 0;
-        _duration = GetDuration(_path[_currentPosition], _path[_currentPosition + 1]);
-        _startTime = Time.time;
-        this.transform.position = _path[_currentPosition].transform.position;
+        this.transform.position = _path[_currentPosition];
     }
-    // Update is called once per frame
     void Update()
     {
         if (_currentPosition < _path.Count - 1)
         {
-            //TODO: Update to move @ constant speeds
-            Move(_path[_currentPosition], _path[_currentPosition + 1]);
-            if (Vector2.Distance(this.transform.position, _path[_currentPosition + 1].transform.position) < 0.006f)
+            Move(_path[_currentPosition + 1], _speed);
+            if (Vector2.Distance(this.transform.position, _path[_currentPosition + 1]) < 0.006f)
             {
                 GetNextTarget();
             }
-
         }
     }
     private void GetNextTarget()
     {
-        _startTime = Time.time;
         _currentPosition++;
-        Debug.Log(_currentPosition);
     }
 
-    private void Move(GameObject aCurrent, GameObject aTarget)
+    private void Move(Vector2 aTarget, float aSpeed)
     {
-        _fraction = (Time.time - _startTime) / _duration;
-        _fraction = Mathf.Clamp01(_fraction);
-        this.transform.position = Vector2.Lerp(aCurrent.transform.position, aTarget.transform.position, _fraction);
-    }
-    private float GetDuration(GameObject aCurrent, GameObject aTarget)
-    {
-        float lDistance = Vector2.Distance(aCurrent.transform.position, aTarget.transform.position);
-        return lDistance / _speed;
+        transform.position = Vector2.MoveTowards(transform.position, aTarget, Time.deltaTime * aSpeed);
     }
 }
--- Assets/Scripts/BloonPathCreator.cs
+++ Assets/Scripts/BloonPathCreator.cs
@@ -10,15 +10,22 @@ public class BloonPathCreator : MonoBehaviour
     [SerializeField] private GameObject _bloonPath;
     [SerializeField] private GameObject _waypointPrefab;
     [SerializeField] private List<GameObject> _pathList;
+    [SerializeField] private List<Vector2> _pathVectors;
 
+    private void Start()
+    {
+        _pathVectors = GeneratePathVectors();
+    }
+
+    #region EDITOR GIZMOS
     public void CreateNewPoint()
     {
         GameObject lTempObj = Instantiate(_waypointPrefab, _bloonPath.transform);
         lTempObj.name = ("Waypoint " + (_pathList.Count + 1).ToString());
-        EditorGUIUtility.SetIconForObject(_waypointPrefab, (Texture2D) EditorGUIUtility
+        EditorGUIUtility.SetIconForObject(_waypointPrefab, (Texture2D)EditorGUIUtility
                 .IconContent("Packages/com.unity.timeline/Editor/StyleSheets/Images/DarkSkin/TimelineAutokey.png").image);
         _pathList.Add(lTempObj);
-    }    
+    }
     public void CreateNewPoint(GameObject aGameObject, Vector2 aPostion)
     {
         GameObject lTempObj = Instantiate(_waypointPrefab, aGameObject.transform);
@@ -30,52 +37,81 @@ public class BloonPathCreator : MonoBehaviour
     public void DeletePath()
     {
         _pathList = new List<GameObject>();
-        for(int i = _bloonPath.transform.childCount - 1; i >= 0; i--)
+        for (int i = _bloonPath.transform.childCount - 1; i >= 0; i--)
         {
             GameObject lChild = _bloonPath.transform.GetChild(i).gameObject;
             DestroyImmediate(lChild);
         }
     }
-
     private void OnDrawGizmos()
     {
-        if(_pathList.Count > 1)
+        if (_pathList.Count > 1)
         {
             for (int i = 0; i < _pathList.Count - 1; i++)
             {
-                if (_pathList[i].transform.childCount > 0)//draw lines to anchor
+                if (_pathList[i].transform.childCount > 0)
                 {
-                    for (int j = _pathList[i].transform.childCount - 1; j >= 0; j--)
-                        DrawLine(_pathList[i].transform, _pathList[i].transform.GetChild(j));
+                    ConnectAnchors(_pathList[i].transform);
                 }
                 if (_pathList[i].GetComponent<Waypoint>()._isBezier && _pathList[i + 1].GetComponent<Waypoint>()._isBezier)
                 {
-                    //draw curve
                     DrawBezierCurve(_pathList[i].transform, _pathList[i + 1].transform);
                 }
                 else
                 {
-                    //draw line
                     DrawLine(_pathList[i].transform, _pathList[i + 1].transform);
                 }
             }
+            //checks the last item in a list for children(anchors)
+            if (_pathList[_pathList.Count - 1].transform.childCount > 0)
+                ConnectAnchors(_pathList[_pathList.Count - 1].transform);
         }
     }
-    private void DrawBezierCurve(Transform aWaypointA, Transform aWaypointB)
+    private void DrawBezierCurve(Transform aStartPoint, Transform aEndPoint)
     {
         Vector2 lGizmoPosition;
-        for(float i = 0; i <= 1; i += 0.1f)
+        for (float i = 0.2f; i < 1; i += 0.2f)
         {
-            lGizmoPosition = CalculateBezierPoint(i, aWaypointA, aWaypointB);
+            lGizmoPosition = CalculateBezierPoint(i, aStartPoint, aEndPoint);
+            Gizmos.color = Color.red;
             Gizmos.DrawSphere(lGizmoPosition, 0.05f);
         }
     }
     private void DrawLine(Transform aWaypointA, Transform aWaypointB)
     {
+        Gizmos.color = Color.white;
         if (aWaypointB != null)
             Gizmos.DrawLine(aWaypointA.position, aWaypointB.position);
+    } 
+    private void ConnectAnchors(Transform aParent)
+    {
+        for (int i = aParent.childCount - 1; i >= 0; i--)
+            DrawLine(aParent, aParent.GetChild(i).transform);
     }
+    #endregion
 
+    private List<Vector2> GeneratePathVectors()
+    {
+        List<Vector2> lPath = new();
+        for(int i = 0;  i < _pathList.Count; i++)
+        {
+            lPath.Add(_pathList[i].transform.position);
+            if(_pathList[i].GetComponent<Waypoint>()._isBezier && _pathList[i + 1].GetComponent<Waypoint>()._isBezier)
+            {
+                lPath = AddBezierVectors(lPath, _pathList[i].transform, _pathList[i + 1].transform);
+            }
+        }
+        return lPath;
+    }
+    private List<Vector2> AddBezierVectors(List<Vector2> aPath, Transform aStartPoint, Transform aEndPoint)
+    {
+        List<Vector2> lBezierPath = aPath;
+        for (float i = 0.2f; i < 1; i += 0.2f)
+        {
+            lBezierPath.Add(CalculateBezierPoint(i, aStartPoint, aEndPoint));
+        }
+        return lBezierPath;
+    }
     private Vector2 CalculateBezierPoint(float aStep, Transform aWaypointA, Transform aWaypointB)
     {
         Vector2 lGizmoPosition = Mathf.Pow(1 - aStep, 3) * aWaypointA.position + 
@@ -83,5 +119,9 @@ public class BloonPathCreator : MonoBehaviour
             3 * (1 - aStep) * Mathf.Pow(aStep, 2) * aWaypointB.GetChild(1).transform.position + 
             Mathf.Pow(aStep, 3) * aWaypointB.position;
         return lGizmoPosition;
+    }
+    public List<Vector2> GetPathVectors()
+    {
+        return _pathVectors;
     }
 }
