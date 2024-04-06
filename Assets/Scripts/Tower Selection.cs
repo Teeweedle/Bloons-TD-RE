b2ada8a29e2224b856e3dc334df5868b07a8aeba
Teeweedle <102382922+Teeweedle@users.noreply.github.com>
2024-04-06 19:27:08 -0400

    Updated art assets
    Updated art assets to have consistent resolution.

---
+++ Assets/Scripts/Tower Selection.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TowerSelection : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //this.transform.GetChild(0).gameObject.SetActive(true);
+    }
+}
