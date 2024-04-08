7f1aee7812c79fd8a6f71722db7b09d6c96b01de
Teeweedle <102382922+Teeweedle@users.noreply.github.com>
2024-04-08 13:04:59 -0400

    Removed assets/ added selection logic
    Removed unused assets and added logic to the towers to show when one is selected in the UI.

--- Assets/Scripts/Tower Selection.cs
+++ Assets/Scripts/Tower Selection.cs
@@ -1,12 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class TowerSelection : MonoBehaviour, IPointerClickHandler
+public class TowerSelected : MonoBehaviour, IPointerClickHandler
 {
+    private static TowerSelected _instance;
+
+    private void Start()
+    {
+        DeselectImage();
+    }
+    /// <summary>
+    /// Hide item selected image
+    /// </summary>
+    private void DeselectImage()
+    {
+        this.transform.GetChild(0).gameObject.SetActive(false);
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
-        //this.transform.GetChild(0).gameObject.SetActive(true);
+        if(_instance != null)
+            _instance.DeselectImage();
+        
+        this.transform.GetChild(0).gameObject.SetActive(true);
+        _instance = this;
+       
     }
 }
