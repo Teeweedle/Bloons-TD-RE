40df5f6facabc53e113eaa204c748e32a1d01a27
Teeweedle <102382922+Teeweedle@users.noreply.github.com>
2024-04-19 09:21:52 -0400

    Tower placement update
    Added visual to see tower range before passing. Pass tower data to tower after it is placed.

--- Assets/Scripts/BaseTower.cs
+++ Assets/Scripts/BaseTower.cs
@@ -5,20 +5,13 @@ using UnityEngine;
 
 public class BaseTower : MonoBehaviour
 {
-    public int _dmg;
-    public int _pierce;
-    public int _range;
+    public string _type;
+    public int _dmg, _pierce, _range;
     public float _attackSpeed;
     public bool _hasCamoDetection;
 
     private void Start()
     {
-        _dmg = 0;
-        _pierce = 0;
-        _range = 0;
-        _attackSpeed = 0;
-        _hasCamoDetection = false;
-        LoadTowerData("Dart Monkey", "No Upgrades");
     }
     private void Fire(GameObject aProjectile, int aAmount)
     {
@@ -30,29 +23,14 @@ public class BaseTower : MonoBehaviour
     private void TargetBloon(string aTargetType)
     {
 
-    }   
-    private void LoadTowerData(string aTowerType, string aTowerName)
-    {
-        try
-        {
-            string lTowerPath = Path.Combine(Application.dataPath, $"Tower Data/{aTowerType}/{aTowerName}.json");
-            string lJsonString = File.ReadAllText(lTowerPath);
-
-            TowerDataObject lTowerObject = JsonUtility.FromJson<TowerDataObject>(lJsonString);
-            UpdateTowerStats(lTowerObject);
-        }
-        catch (FileNotFoundException ex)
-        {
-            Debug.LogError($"File not found: {ex.Message}");
-        }
-        
     }
-    private void UpdateTowerStats(TowerDataObject aTowerObject)
+    public void AssignStats(TowerDataObject aData)
     {
-        _dmg = aTowerObject.damage;
-        _pierce = aTowerObject.pierce;
-        _range = aTowerObject.range;
-        _attackSpeed = aTowerObject.attackSpeed / 100;
-        _hasCamoDetection = aTowerObject.hasCamoDetection;
+        _type = aData.type;
+        _attackSpeed = aData.attackSpeed;
+        _dmg = aData.damage;
+        _pierce = aData.pierce;
+        _range = aData.range;
+        _hasCamoDetection = aData.hasCamoDetection;
     }
 }
--- Assets/Scripts/PlaceTower.cs
+++ Assets/Scripts/PlaceTower.cs
@@ -1,9 +1,25 @@
+using System.IO;
 using UnityEngine;
 
 public class PlaceTower : MonoBehaviour
 {
     [SerializeField] SpriteRenderer _spriteRenderer;
+    [SerializeField] GameObject _towerRange;
+
+    private readonly string _DELIMITER = "Prefab";
+    private readonly string _TOWERTYPE = "No Upgrades";
+
     private bool _canPlace = true;
+    private GameObject _towerRangeGO;
+    private BaseTower _tower;
+    private TowerDataObject _towerData = new();
+
+    private void Start()
+    {
+        _towerData = LoadTowerData(GetTowerName(this.gameObject.name, _DELIMITER), _TOWERTYPE);
+        _tower = GetComponent<BaseTower>();
+        DisplayTowerRange();
+    }
     void Update()
     {        
         FollowMousePosition();
@@ -12,12 +28,12 @@ public class PlaceTower : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(0))
             {
-                this.gameObject.AddComponent<BaseTower>();
+                _tower.AssignStats(_towerData);
+                Destroy(_towerRangeGO);
                 Destroy(this);
             } 
         }
     }
-
     private void FollowMousePosition()
     {
         transform.position = Camera.main.ScreenToWorldPoint(GetMousePosition());
@@ -38,5 +54,39 @@ public class PlaceTower : MonoBehaviour
     {
         _canPlace = true;
         _spriteRenderer.color = Color.white;
+    }
+    private void DisplayTowerRange()
+    {
+        _towerRangeGO = Instantiate(_towerRange, this.transform);
+        _towerRangeGO.transform.position = this.transform.position;
+        _towerRangeGO.transform.localScale = _towerRangeGO.transform.localScale * _towerData.range;
+    }
+    private string GetTowerName(string aPrefabName, string aDelimiter)
+    {
+        int lDelimiterIndex = aPrefabName.IndexOf(aDelimiter);
+        if (lDelimiterIndex != -1)
+        {
+            return aPrefabName.Substring(0, lDelimiterIndex).Trim();
+        }
+        else
+        {
+            return aPrefabName;
+        }
+    }
+    private TowerDataObject LoadTowerData(string aTowerName, string aTowerType)
+    {
+        TowerDataObject lTowerObject = new();
+        try
+        {
+            string lTowerPath = Path.Combine(Application.dataPath, $"Tower Data/{aTowerName}/{aTowerType}.json");
+            string lJsonString = File.ReadAllText(lTowerPath);
+
+            lTowerObject = JsonUtility.FromJson<TowerDataObject>(lJsonString);
+        }
+        catch (FileNotFoundException ex)
+        {
+            Debug.LogError($"File not found: {ex.Message}");
+        }
+        return lTowerObject;
     }
 }
